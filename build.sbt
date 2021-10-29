9cd640bc243d8be845a7c7389c8e7b737211f9d8
Sonali181 <sonali.kumari@knoldus.com>
2021-10-29 10:03:11 +0530

    first commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "Assignment_GenericStack"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % Test
+
---
+++ src/test/scala/TestCase.scala
@@ -0,0 +1,28 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestCase extends AnyFunSuite{
+
+  val alistStack = new StackImpl[Int](List(12,45,78,84,76))
+  val emptyStack = new StackImpl[Int](List())
+
+  test("Element should pushed in a Stack"){
+    assert(alistStack.push(64).head == 64)
+  }
+
+  test("Poped Element should equals to first element of Stack"){
+    assert(alistStack.pop.canEqual(12))
+  }
+
+  test("Stack is empty"){
+    assert(emptyStack.isEmpty === true)
+  }
+
+  test("Stack is not empty"){
+    assert(alistStack.isEmpty === false)
+  }
+
+
+//  test("Top element should be the head of the Stack"){
+//    assert(alistStack.top == 12)
+//  }
+}
